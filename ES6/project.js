5374ee18fe18b9701d96ebcac56e75b426d5054f
mehmet ali <67506638+lckhndleup@users.noreply.github.com>
2023-01-27 13:41:22 +0300

    Update project.js

--- ES6/project.js
+++ ES6/project.js
@@ -1,7 +1,3 @@
-//ilk olarak formumuzu seçmemiz gerekiyor."film ekleyin" butonuna bastıgımız zaman film eklemeye çalışıcaz.bunu ana js dosyamız olan project.js de yapıcaz.
-
-//formu seçme (film ekleyin butonuna basıdıgında film eklemek için form seçiyoruz.)
-
 const form = document.querySelector("#film-form");
 
 //3 tane input alanını seçme(film ismi , yönetmen, film afiş linki)
@@ -15,30 +11,23 @@ const urlElement = document.querySelectorAll(".form-control")[2]; //film afiş l
 const secondCardBody = document.querySelectorAll(".card-body")[1]; 
 
 const clearButton = document.querySelector("#clear-films");
-//arayüze film ekleme yapmak istediğimiz için bir tane UI Constructor a ihtiyacımız var.arayüz işlemlerimizi bu consturctor da yapıcaz.(ui.js de yapıcaz.)
-//------------------------------------------------------------------------------------------
 
 //------------------------------------------------------------------------------------------
-//tüm eventleri yüklemek için bir fonksiyon yazıcaz.(aynı önceki projede yaptıgımız gibi eventListenersları bir fonksiyon altında toplucaz.)
+
+
+
 
 eventListeners();
 function eventListeners (){
-    //ilk olarak formumuza submit event i katmamız gerekiyor.form submit oldugunda bu event oluşsun.
     form.addEventListener("submit",addFilm); //form submit oldugunda addFilm fonksiyonu çalışsın.
-    //-------------------------------------------
-    //sayfa  yüklendiğinde filmlerin arayüzde kalması için DOMContentLoaded() eventini eklememiz gerekiyor.
     document.addEventListener("DOMContentLoaded",function(){ 
-        //fonksiyonu ekstra olarak yazmadık, direk burda yazıcaz.
-        //yapmamız gerekn şey:local storageden arrayimizi almamız gerekiyor.
-        //getFilmsFromStorage() dan arrayimizi alıcaz.
+        
         let films = Storage.getFilmsFromStorage();//array i dönecek.
-        //bu array i ui ın içindeki loadAllFilms() fonksiyonuna göndericez.
+        
         UI.loadAllFilms(films);
 
     }); 
-    //-------------------------------------------
-    //filmleri arayüzden silme için event delegation yada event capturing özelliğini kullanıcaz.cardbody e click event i atıcaz. event target ile alıcaz.(2 tane card body var 2ncisini alıcaz.)
-    //card-body i seçelim:
+    
     
 
     secondCardBody.addEventListener("click",deleteFilm);
@@ -55,23 +44,21 @@ function eventListeners (){
 //film ekleme fonksiyonu:
 
 function addFilm (event){
-    //bu fonksiyonda ilk olarak yapmamız gereken şey şu : 3 adet inputumuz vardı bunları yukarda seçtik.(film ismi=titleElement,yönetmen=directorElement,film afiş=urlElement) bu inputların değerlerini alalım.
+    
 
     //input değerlerini alma (value ile alıyoruz):
     const title = titleElement.value;
     const director = directorElement.value;
     const url = urlElement.value;
 
-    //sıradaki işlen şu olmalı >>> eğer bu inputlardan bir tanesi bile eksik ise hata mesajı gelsin istiyoruz.
+    
 
     if(title === "" || director === "" || url === ""){  // ("||" >>> veya()  
         //hata mesajı:
         UI.displayMessages("Tüm Alanları Doldurun!!!","danger");
     }
     else{
-        //eğer 3 tane inputta dolu ise film oluşturmaya çalısıcaz.ve filmimizi "Film" Constructordan oluşturcaz.
-
-        //Film const. dan obje oluşturalım.
+        
         //Yeni Film:
         const newFilm = new Film(title,director,url); //Film const. ı 3 tane arguman alıyordu.bu değerleri title, director  ve url olacak.
 
@@ -91,14 +78,14 @@ function addFilm (event){
 
         //-------------------------------
     }
-    //film eklendikten hemen sonra bu fonksiyonu çağırıyoruz ki input alanları boşalsın . 
-    UI.clearInputs(titleElement,directorElement,urlElement); //argümanlar input alanlarımız olacak. dikkat!!! title, director ve url değil. onlar value değerleri bizim istediğimiz input alanlarıdır.
+    
+    UI.clearInputs(titleElement,directorElement,urlElement); 
 
 
 
 
 
-    event.preventDefault();//formun submit edilmesini önlemek için yazdıgımız fonksiyon.(sayfayı tekrardan yüklüyordu.bunu engelliyoruz.yani default olan değerini engelliyoruz.)
+    event.preventDefault();
 }
 
 //------------------------------------------------------------------------------------------
